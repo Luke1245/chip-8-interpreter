5cc334a2fd2608fabd317693312b2af3b625c836
Luke <124802535+Luke1245@users.noreply.github.com>
2026-08-21 12:48:51 +0100

    More comments and slight cleanup

--- src/main.c
+++ src/main.c
@@ -20,10 +20,10 @@ typedef struct config_type {
 
 bool set_config(config_t* config, int argc, char* argv[]) {
     // Set defaults
-    config->window_width = 64;
-    config->window_height = 32;
-    config->fg_colour = 0xFFFFFFFF;  // WHITE
-    config->bg_colour = 0xFF0000FF;  // RED
+    config->window_width = 64;       // Original CHIP-8 resolution
+    config->window_height = 32;      // Original CHIP-8 resolution
+    config->fg_colour = 0xFFFFFFFF;  // RGBA8888
+    config->bg_colour = 0xFF0000FF;  // RGBA8888
     config->scale_factor = 20;
 
     // Supress compiler warnings
@@ -63,29 +63,32 @@ bool initialise_sdl(sdl_t* sdl, config_t config) {
 }
 
 void exit_cleanup(sdl_t* sdl) {
+    // Renderer must be destroyed before window
     SDL_DestroyRenderer(sdl->renderer);
     SDL_DestroyWindow(sdl->window);
     SDL_Quit();
 }
 
 void clear_screen(const config_t config, const sdl_t sdl) {
+    // Bit shifts and masks to extract relevant channels from 32-bit config
+    // integer
     const uint8_t r = (config.bg_colour >> 24) & 0xFF;
     const uint8_t g = (config.bg_colour >> 16) & 0xFF;
     const uint8_t b = (config.bg_colour >> 8) & 0xFF;
+    // Shift not needed but keeps consistent with style
     const uint8_t a = (config.bg_colour >> 0) & 0xFF;
 
     SDL_SetRenderDrawColor(sdl.renderer, r, g, b, a);
     SDL_RenderClear(sdl.renderer);
 }
 
+// This will do more later, currently wrapper
 void update_screen(const sdl_t sdl) { SDL_RenderPresent(sdl.renderer); }
 
 int main(int argc, char* argv[]) {
-    (void)argc;
-    (void)argv;
-
     sdl_t sdl = {0};
     config_t config = {0};
+    SDL_Event event = {0};
 
     // Ensure that config is correctly set
     if (!set_config(&config, argc, argv)) {
@@ -97,9 +100,9 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    SDL_Event event = {0};
     // Main loop
     while (true) {
+        // Allow user to quit window
         SDL_PollEvent(&event);
         if (event.type == SDL_EVENT_QUIT) {
             break;

242486bb23e6d5bcf32477437fb1ae7c31c80d92
Luke <124802535+Luke1245@users.noreply.github.com>
2026-08-21 12:22:21 +0100

    Formatting using google C style

--- src/main.c
+++ src/main.c
@@ -1,61 +1,68 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h> 
-#include <stdint.h>
-
 #include <SDL3/SDL.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct sdl_type {
-    SDL_Window *window;
-    SDL_Renderer *renderer; 
+    SDL_Window* window;
+    SDL_Renderer* renderer;
 } sdl_t;
 
 typedef struct config_type {
     uint32_t window_width;
     uint32_t window_height;
-    uint32_t fg_colour; // Foreground colour 
-    uint32_t bg_colour; // Background colour
-    uint32_t scale_factor; // Amount to scale CHIP-8 pixel by (Original resolution is too small for modern displays)
+    uint32_t fg_colour;     // Foreground colour
+    uint32_t bg_colour;     // Background colour
+    uint32_t scale_factor;  // Amount to scale CHIP-8 pixel by (Original
+                            // resolution is too small for modern displays)
 } config_t;
 
-bool set_config(config_t *config, int argc, char * argv[]) {
+bool set_config(config_t* config, int argc, char* argv[]) {
     // Set defaults
     config->window_width = 64;
     config->window_height = 32;
-    config->fg_colour = 0xFFFFFFFF; // WHITE 
-    config->bg_colour = 0xFF0000FF; // RED 
+    config->fg_colour = 0xFFFFFFFF;  // WHITE
+    config->bg_colour = 0xFF0000FF;  // RED
     config->scale_factor = 20;
 
-    // Supress compiler warnings 
+    // Supress compiler warnings
     // TODO: Implement command line arguments
     for (int i = 1; i < argc; i++) {
-        (void)argv[i]; 
+        (void)argv[i];
     }
 
     return true;
 }
 
-bool initialise_sdl(sdl_t *sdl, config_t config) {
+bool initialise_sdl(sdl_t* sdl, config_t config) {
     if (!SDL_SetAppMetadata("CHIP-8 Interpreter", NULL, NULL)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failure setting SDL metadata %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Failure setting SDL metadata %s\n", SDL_GetError());
         return false;
     }
 
     if (!SDL_InitSubSystem(SDL_INIT_AUDIO | SDL_INIT_VIDEO)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failure initialising SDL subsytems %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Failure initialising SDL subsytems %s\n", SDL_GetError());
         return false;
-    } 
+    }
 
-    SDL_CreateWindowAndRenderer("CHIP-8 Interpreter", config.window_width * config.scale_factor, config.window_height * config.scale_factor, 0, &(sdl->window), &(sdl->renderer));
+    SDL_CreateWindowAndRenderer("CHIP-8 Interpreter",
+                                config.window_width * config.scale_factor,
+                                config.window_height * config.scale_factor, 0,
+                                &(sdl->window), &(sdl->renderer));
     if (!sdl->window || !sdl->renderer) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failure creating SDL window and renderer %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Failure creating SDL window and renderer %s\n",
+                     SDL_GetError());
         return false;
     }
 
     return true;
 }
 
-void exit_cleanup(sdl_t *sdl) {
+void exit_cleanup(sdl_t* sdl) {
     SDL_DestroyRenderer(sdl->renderer);
     SDL_DestroyWindow(sdl->window);
     SDL_Quit();
@@ -71,11 +78,9 @@ void clear_screen(const config_t config, const sdl_t sdl) {
     SDL_RenderClear(sdl.renderer);
 }
 
-void update_screen(const sdl_t sdl) {
-    SDL_RenderPresent(sdl.renderer);
-}
+void update_screen(const sdl_t sdl) { SDL_RenderPresent(sdl.renderer); }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
 
@@ -87,7 +92,7 @@ int main(int argc, char * argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Ensure SDL initialises succesfully 
+    // Ensure SDL initialises succesfully
     if (!initialise_sdl(&sdl, config)) {
         exit(EXIT_FAILURE);
     }

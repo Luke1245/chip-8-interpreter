85451802da64aa7ccf5a3b5cc44a529dcac68605
Luke <124802535+Luke1245@users.noreply.github.com>
2026-09-08 20:40:00 +0100

    Set carry flag to zero on bitwise AND, OR, and XOR
    The original CHIP-8 implementation of bitwise AND, OR, and XOR also
    sets the carry flag in register vF to zero.

--- src/chip8.c
+++ src/chip8.c
@@ -193,6 +193,8 @@ void emulate_instruction(chip8_t* chip8, const config_t* config) {
                 case 0x1:
                     // 0x8XY1: Sets VX to VX bitwise OR VY
                     chip8->V[chip8->inst.X] |= VY;
+                    // Original CHIP-8 spec resets the flag register to zero
+                    chip8->V[0xF] = 0x0;
 
                     DEBUG_PRINT(
                         "Set V%X (0x%02X) to bitwise OR V%X (0x%02X): RES = "
@@ -204,6 +206,8 @@ void emulate_instruction(chip8_t* chip8, const config_t* config) {
                 case 0x2:
                     // 0x8XY2: Sets VX to VX bitwise AND VY
                     chip8->V[chip8->inst.X] &= VY;
+                    // Original CHIP-8 spec resets the flag register to zero
+                    chip8->V[0xF] = 0x0;
 
                     DEBUG_PRINT(
                         "Set V%X (0x%02X) to bitwise AND V%X (0x%02X): RES = "
@@ -215,6 +219,8 @@ void emulate_instruction(chip8_t* chip8, const config_t* config) {
                 case 0x3:
                     // 0x8XY3: Sets VX to VX bitwise XOR VY
                     chip8->V[chip8->inst.X] ^= VY;
+                    // Original CHIP-8 spec resets the flag register to zero
+                    chip8->V[0xF] = 0x0;
 
                     DEBUG_PRINT(
                         "Set V%X (%02X) to bitwise XOR V%X (%02X): RES = "
